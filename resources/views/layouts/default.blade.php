b48ebf55aa1dff5b8c393bc4074e4a3d45b07920
vagrant <vagrant@homestead>
2019-04-01 09:06:10 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>@yield('title','Weibo App')- Laravel 教程</title>
+</head>
+
+<body>
+  @yield('content')
+</body>
+
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title','主页')
+
+@section('content')
+<h1>我是主页</h1>
+@endsection
