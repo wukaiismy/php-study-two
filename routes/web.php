e177397bb17b4f2af7f714fcdc3dc09389b64b5c
vagrant <vagrant@homestead>
2019-04-02 02:33:42 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+/**
+ * 用户登录注册相应路由
+ */
+Route::get('signup', 'UsersController@create')->name('signup');
+ 
