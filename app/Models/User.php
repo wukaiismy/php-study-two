0423a69ea34278a1eb3308d12f8b2721ade96b91
vagrant <vagrant@homestead>
2019-04-10 02:50:13 +0000

    用户激活

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,15 @@ class User extends Authenticatable
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+
+    /**
+     * 生成邮箱激活令牌
+     */
+    public static  function boot()
+    {
+        parent::boot();
+        static::creating(function ($user) {
+            $user->activation_token = str_random(30);
+        });
+    }
 }
