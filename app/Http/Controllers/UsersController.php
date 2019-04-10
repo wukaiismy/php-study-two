d8aa483a3765f422ca1d00b6b2b10601de89888c
vagrant <vagrant@homestead>
2019-04-10 03:35:50 +0000

    移除邮件大宋的from选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -111,13 +111,12 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'wukaiismyh@gmail.com';
-        $name = 'Wukai';
+
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
     /**
