47669e5a2398df3a20f1284cc250c80510830a53
vagrant <vagrant@homestead>
2019-04-15 08:20:54 +0000

    删除微博

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -9,12 +9,13 @@ class AuthServiceProvider extends ServiceProvider
 {
     /**
      * The policy mappings for the application.
-     *
+     *  授权策略配置
      * @var array
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
         \App\Models\User::class => \App\Policies\UserPolicy::class,
+        \App\Models\Status::class  => \App\Policies\StatusPolicy::class,
     ];
 
     /**
